18941d0f7b4f540fd3eceae2210d59ec3ea4d9a9
Sokratis Dimitriadis <info@28x.gr>
2025-09-27 20:52:44 +0800

    Robot arm basic urdf model created (no xacro)

---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sokratis/robotic_arm_6dof_ws/src/my_robot_description
+# Build directory: /home/sokratis/robotic_arm_6dof_ws/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
